79f7677f0fbae10a71dba5cf31645ba192c4d880
Nina Johnson <9makesthings@gmail.com>
2019-08-13 13:37:59 -0500

    rough home layout

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {HashRouter as Router} from 'react-router-dom';
 import './App.css';
+
 import Nav from '../Nav/Nav';
+import Home from '../Home/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
+
+        <Home />
 
 
       </div>
