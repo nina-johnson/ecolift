6cda7301ca76112d04385e099d5570fdbba19fce
Nina Johnson <9makesthings@gmail.com>
2019-08-13 12:21:05 -0500

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header > {/* className="App-header" */}
+        <h1>ECOLIFT</h1>
+        <h5>Concrete Raising & Leveling</h5>
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
