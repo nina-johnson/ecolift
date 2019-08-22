c44c94cb45a80eb50c97f4c32fd4d38e145873b4
Nina Johnson <9makesthings@gmail.com>
2019-08-22 13:55:47 -0500

    animated scrolling to home on click of logo in navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 // import { Link, withRouter } from 'react-router-dom';
 import './Nav.css';
 
 const Nav = (props) => {
 
+    let scrollToHome = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
     let scrollToLocation = () => {
-        alert('Clicked! Need to add animation function here.')
+        // const loc = ReactDOM.findDOMNode('#contact');
+
+        // console.log( loc );
+        
+        window.scrollTo({
+            behavior: 'smooth'
+        })
+    }
+
         // $('.navbar-collapse ul li a').on('click', function() {  
         //     $('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
         //     return false;
         // });
-    }
-
 
     return (
         <div className="nav" >
 
-            <a className="nav-title" href="#home" > {/* className="App-header" */}
+            <div className="nav-title" href="#home" onClick={scrollToHome} > {/* className="App-header" */}
                 <img src="images/EcoLift_logo.png" alt="EcoLift" />
                 {/* <h5>Concrete Raising & Leveling</h5> */}
-            </a>
+            </div>
 
             <div className="nav-right" >
                 <a className="nav-link" href="#services" >
@@ -30,7 +44,7 @@ const Nav = (props) => {
                     About Mudjacking
                 </a>
 
-                <a className="nav-link" href="#contact" onClick={scrollToLocation} >
+                <a className="nav-link" href="#contact" onClick={scrollToLocation()} >
                     Contact Us
                 </a>
 
